132f1e1bd4b5f7e429fe71ec06ba7bd2949fdba1
Anastasiya Karaman <nastyakoc97@gmail.com>
2023-03-12 19:01:03 +0300

    added sending email to new user after registration

--- News_Portal/settings.py
+++ News_Portal/settings.py
@@ -150,3 +150,13 @@ ACCOUNT_AUTHENTICATION_METHOD = 'email'
 ACCOUNT_EMAIL_VERIFICATION = 'none'
 
 ACCOUNT_FORMS = {"signup": "accounts.forms.CustomSignupForm"}
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.yandex.ru'
+EMAIL_PORT = 465
+EMAIL_HOST_USER = "anastaciakaraman@yandex.ru"
+EMAIL_HOST_PASSWORD = "hvicsvrkisqfswtk"
+EMAIL_USE_TLS = False
+EMAIL_USE_SSL = True
+
+DEFAULT_FROM_EMAIL = "anastaciakaraman@yandex.ru"
--- accounts/forms.py
+++ accounts/forms.py
@@ -3,6 +3,7 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from allauth.account.forms import SignupForm
 from django.contrib.auth.models import Group
+from django.core.mail import send_mail
 
 class SignUpForm(UserCreationForm):
     email = forms.EmailField(label="Email")
@@ -19,6 +20,14 @@ class SignUpForm(UserCreationForm):
 class CustomSignupForm(SignupForm):
     def save(self, request):
         user = super().save(request)
+        #добавление пользователя в группу
         common_users = Group.objects.get(name="common users")
         user.groups.add(common_users)
+        #отправка на почту
+        send_mail(
+            subject='Добро пожаловать на новостной портал!',
+            message=f'{user.username}, вы успешно зарегистрировались!',
+            from_email=None,  # будет использовано значение DEFAULT_FROM_EMAIL
+            recipient_list=[user.email],
+        )
         return user
