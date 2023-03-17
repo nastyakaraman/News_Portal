7d422f6ea3f7e72896e9fb2e7ebbdbec6e7e2172
Anastasiya Karaman <nastyakoc97@gmail.com>
2023-03-17 18:05:32 +0300

    fixed weekly email distribution: now it only suggests posts  in subdcriber's fav categories

--- news/admin.py
+++ news/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Post, PostCategory, Author, Comment
+from .models import Post, PostCategory, Author, Comment, Category
 
 admin.site.register(Post)
 admin.site.register(PostCategory)
 admin.site.register(Author)
 admin.site.register(Comment)
+admin.site.register(Category)
--- news/management/commands/runapscheduler.py
+++ news/management/commands/runapscheduler.py
@@ -13,6 +13,7 @@ from django.template.loader import render_to_string
 
 
 from news.models import Post, Subscription
+from django.contrib.auth.models import User
 
 logger = logging.getLogger(__name__)
 
@@ -21,21 +22,25 @@ def my_job():
     lastweek = today - datetime.timedelta(days=7)
     posts = Post.objects.filter(time_in__gte=lastweek)
     categories = set(posts.values_list('category__id', flat = True))
-    subscribers = set(Subscription.objects.filter(category__in=categories).values_list('user__email', flat = True))
-
-    html_content= render_to_string(
-        'weekly_news.html',
-        {
-             'posts': posts,
-        }
-    )
-
+    emailed=[]
+    subscribers = set(Subscription.objects.filter(category__in=categories).values_list('user__email', flat=True))
     for subscriber in subscribers:
+        user_id = User.objects.get(email=subscriber).id
+        categories_for_subscriber = set(Subscription.objects.filter(user=user_id).values_list('category__id', flat=True))
+        posts_for_subscriber = posts.filter(category__in = categories_for_subscriber)
+        html_content = render_to_string(
+            'weekly_news.html',
+            {
+                'posts': posts_for_subscriber,
+            }
+        )
+
         msg = EmailMultiAlternatives(
-            subject='Еженедельная рассылка',
-            body='',
-            from_email=settings.DEFAULT_FROM_EMAIL,
-            to=[subscriber])
+        subject='Еженедельная рассылка по любимым категориям',
+        body='',
+        from_email=settings.DEFAULT_FROM_EMAIL,
+        to=[subscriber]
+        )
         msg.attach_alternative(html_content, "text/html")
         msg.send()
 
@@ -53,7 +58,7 @@ class Command(BaseCommand):
 
         scheduler.add_job(
             my_job,
-            trigger=CronTrigger(day_of_week="fri", hour="16", minute="59"),
+            trigger=CronTrigger(day_of_week="fri", hour="19", minute="00"),
             id="my_job",  # The `id` assigned to each job MUST be unique
             max_instances=1,
             replace_existing=True,
