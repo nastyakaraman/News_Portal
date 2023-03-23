3e46f405d2588492cfe8b8f0cfc59f45ded8d26b
Anastasiya Karaman <nastyakoc97@gmail.com>
2023-03-23 23:14:02 +0300

    added weekly email distribution via celery

--- News_Portal/celery.py
+++ News_Portal/celery.py
@@ -1,6 +1,8 @@
 import os
 from celery import Celery
 from celery.schedules import crontab
+import news
+# news.tasks import send_weeklynews
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'News_Portal.settings')
 
@@ -9,12 +11,14 @@ app.config_from_object('django.conf:settings', namespace='CELERY')
 
 app.autodiscover_tasks()
 
-'''app.conf.beat_schedule = {
+app.conf.timezone = 'Europe/London'
+app.conf.enable_utc = True
+
+app.conf.beat_schedule = {
     'action_every_30_seconds': {
-        'task': 'tasks.action',
-        'schedule': crontab(minute=0, hour=8,
-                            day_of_week='monday')
-        'args': (args),
+        'task': 'news.tasks.send_weeklynews',
+        'schedule': crontab(minute=0, hour=5,
+                            day_of_week='monday'),
     },
-}'''
+}
 
--- news/tasks.py
+++ news/tasks.py
@@ -1,9 +1,10 @@
 from celery import shared_task
 from django.core.mail import EmailMultiAlternatives
 from .models import Post, Subscription
+from django.contrib.auth.models import User
 from django.conf import settings
-import time
-
+from celery import Celery
+from django.template.loader import render_to_string
 
 @shared_task
 def send_newarticle(oid):
@@ -26,3 +27,34 @@ def send_newarticle(oid):
                 )
             msg.attach_alternative(html_content, "text/html")
             msg.send()
+
+app = Celery('News_Portal')
+@app.task
+def send_weeklynews():
+    import datetime
+    today = datetime.datetime.now()
+    lastweek = today - datetime.timedelta(days=7)
+    posts = Post.objects.filter(time_in__gte=lastweek)
+    categories = set(posts.values_list('category__id', flat=True))
+    emailed = []
+    subscribers = set(Subscription.objects.filter(category__in=categories).values_list('user__email', flat=True))
+    for subscriber in subscribers:
+        user_id = User.objects.get(email=subscriber).id
+        categories_for_subscriber = set(
+            Subscription.objects.filter(user=user_id).values_list('category__id', flat=True))
+        posts_for_subscriber = posts.filter(category__in=categories_for_subscriber)
+        html_content = render_to_string(
+            'weekly_news.html',
+            {
+                'posts': posts_for_subscriber,
+            }
+        )
+
+        msg = EmailMultiAlternatives(
+            subject='Еженедельная рассылка по любимым категориям',
+            body='',
+            from_email=settings.DEFAULT_FROM_EMAIL,
+            to=[subscriber]
+        )
+        msg.attach_alternative(html_content, "text/html")
+        msg.send()
